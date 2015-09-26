fb0ff153ca025323db6491550ab5558c94de9bd0
mehdi <mehdi@cherti.name>
2015-09-26 11:01:34 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="ssapy",
+    version="0.1",
+    author="",
+    author_email="mehdi@cherti.name",
+    description=(""),
+    license="BSD",
+    keywords="",
+    url="",
+    packages=["ssapy"],
+)
